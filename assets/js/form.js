73671b77cea63018abd2c0c48e406ad7e0e54caa
Frederick Erlando <frederick2031179@itpln.ac.id>
2023-05-04 21:59:19 +0700

    modify form

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,4 +5,18 @@ function saveData(){
     const message = document.getElementById('message').value;
     const data = { name, email, subject, message};
     localStorage.setItem('formData', JSON.stringify(data));
+
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('subject').value = '';
+    document.getElementById('message').value = '';
 }
+
+function loadData() {
+    const data = localStorage.getItem('formData');
+    if (data) {
+      return JSON.parse(data);
+    }
+    return null;
+  }
+
