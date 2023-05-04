f36566ba825e82cef24d054c1fbcf85fb5f82496
Frederick Erlando <frederick2031179@itpln.ac.id>
2023-05-04 22:24:00 +0700

    fix form js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,10 +1,13 @@
+let formDataArray = [];
+
 function saveData(){
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const subject = document.getElementById('subject').value;
     const message = document.getElementById('message').value;
     const data = { name, email, subject, message};
-    localStorage.setItem('formData', JSON.stringify(data));
+    formDataArray.push(data); 
+    localStorage.setItem('formDataArray', JSON.stringify(formDataArray));
 
     document.getElementById('name').value = '';
     document.getElementById('email').value = '';
@@ -12,11 +15,9 @@ function saveData(){
     document.getElementById('message').value = '';
 }
 
-function loadData() {
-    const data = localStorage.getItem('formData');
-    if (data) {
-      return JSON.parse(data);
-    }
-    return null;
-  }
+function getData() {
+  const formDataString = localStorage.getItem('formDataArray');
+  const formDataArray = JSON.parse(formDataString);
+  return formDataArray || [];
+}
 
